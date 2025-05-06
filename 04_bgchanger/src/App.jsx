f9ec34ab319dd44ba641df464797b94406241fb2
Your Name <your@email.com>
2025-05-06 19:05:59 +0530

    initial commit

---
+++ 04_bgchanger/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("black");
+
+  return (
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => setColor("red")}
+            className="cursor-pointer outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor("green")}
+            className="cursor-pointer outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor("blue")}
+            className="cursor-pointer outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
+          <button
+            onClick={() => setColor("#F69336")}
+            className="cursor-pointer outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "#F69336" }}
+          >
+            Bhagva
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
