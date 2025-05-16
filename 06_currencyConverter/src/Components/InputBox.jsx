78e7b47489cbf08f260637ed525b38ade85925ea
Sourabh Mane <msourabh631@gmail.com>
2025-05-16 19:28:17 +0530

    third commit

--- 06_currencyConverter/src/Components/InputBox.jsx
+++ 06_currencyConverter/src/Components/InputBox.jsx
@@ -1,5 +1,7 @@
 import React, {useId} from 'react'
 
+
+
 function InputBox({
     label,
     amount,
@@ -15,13 +17,13 @@ function InputBox({
    const amountInputId = useId()
 
     return (
-        <div className={`bg-white p-3 rounded-lg text-sm flex `}>
+        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
-                <label  className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountInputId}  className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
                 <input
-                    
+                    id={amountInputId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
@@ -37,16 +39,11 @@ function InputBox({
                     value={selectCurrency}
                     onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
                     disabled={currencyDisable}
-                    
                 >
                     
-                        {/* <option value="usd">
-                            usd
-                        </option> */}
-                        {/* Curency lop */}
                         {currencyOptions.map((currency) => (
                             <option key={currency} value={currency}>
-                                {currency.toUpperCase()}
+                            {currency}
                             </option>
                         ))}
                 
@@ -55,5 +52,6 @@ function InputBox({
         </div>
     );
 }
+
 
 export default InputBox;
