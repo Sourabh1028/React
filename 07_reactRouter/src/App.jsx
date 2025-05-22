8ce7e19e04fecd2724dca1d9b338b9b139ca6b62
Sourabh Mane <msourabh631@gmail.com>
2025-05-22 20:20:33 +0530

    react router

--- 07_reactRouter/src/App.jsx
+++ 07_reactRouter/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
+import Header from './components/Header/header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-green-200 p-4 text-center'>React Router</h1>
+      <Header />
+      <Home />
+      <Footer />
     </>
   )
 }
--- 07_reactRouter/src/components/Header/headre.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function headre() {
-  return (
-    <div>headre</div>
-  )
-}
-
-export default headre
