629e2c3c080bb899a60f6b71c990b785f06e00ae
Sourabh Mane <msourabh631@gmail.com>
2025-06-13 19:49:44 +0530

    router

---
+++ 07_reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react'
+
+function Github() {
+  const [data, setData] = useState([])  
+  useEffect(() => {
+    fetch('https://api.github.com/users/Sourabh1028')  
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        setData(data)
+    })
+  }, [])
+
+  return (
+    <div className='text-center p-4 text-black'>Github followers: {data.followers}
+
+    <img className='text-center' src={data.avatar_url} alt="Git profile photo" width={300} />
+    </div>
+
+  )
+}
+
+export default Github
--- 07_reactRouter/src/components/User/User.jsx
+++ 07_reactRouter/src/components/User/User.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 function User() {
+  // useParams is a hook that returns an object of key/value pairs of the dynamic params from the current URL
+  const { userid } = useParams();
   return (
-    <div>User:</div>
+    <div className='bg-yellow-700 text-3xl p-4 text-white'>User: {userid}</div>
   )
 }
 
