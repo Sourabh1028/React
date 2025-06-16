29b35a923971ad7a3b3cf6a95ba5e7fafc5d0ca4
Sourabh Mane <msourabh631@gmail.com>
2025-06-16 19:33:16 +0530

    router end

--- 07_reactRouter/src/components/Github/Github.jsx
+++ 07_reactRouter/src/components/Github/Github.jsx
@@ -1,23 +1,30 @@
-import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
-  const [data, setData] = useState([])  
-  useEffect(() => {
-    fetch('https://api.github.com/users/Sourabh1028')  
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        setData(data)
-    })
-  }, [])
+  const data = useLoaderData()  
+
+  // const [data, setData] = useState([])  
+  // useEffect(() => {
+  //   fetch('https://api.github.com/users/Sourabh1028')  
+  //   .then(response => response.json())
+  //   .then(data => {
+  //       console.log(data);
+  //       setData(data)
+  //   })
+  // }, [])
 
   return (
     <div className='text-center p-4 text-black'>Github followers: {data.followers}
 
-    <img className='text-center' src={data.avatar_url} alt="Git profile photo" width={300} />
+    <img src={data.avatar_url} alt="Git profile photo" width={300} />
     </div>
 
   )
 }
 
 export default Github
+
+export const githubInfoLoader = async () => {
+  const response = await fetch('https://api.github.com/users/Sourabh1028')
+  return response.json()
+}
