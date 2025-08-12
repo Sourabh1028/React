5a3e4116a08f7c4fe17a0a8b6fb57a147d08abc9
Sourabh Mane <msourabh631@gmail.com>
2025-08-12 19:42:42 +0530

    todo project

--- 10_todoContextLocal/src/App.jsx
+++ 10_todoContextLocal/src/App.jsx
@@ -7,12 +7,24 @@ import './App.css'
 function App() {
   const [todos, setTodos] = useState([])
 
-  const addTodo = () => {
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), }, ...prev]) //'...' to add previous values
+  }
 
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo === id))
   }
 
   return (
-    <TodoProvider value={{ todos, addTodo, updateTodo, deletetodo, toggleComplete }}>
+    <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
